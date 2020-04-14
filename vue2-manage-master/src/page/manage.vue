24419ae596b17d3ab83b087d9ade6d81fa7cf97c
朱俊颖 <1411565404@qq.com>
2020-04-14 18:35:26 +0800

    测试

---
+++ vue2-manage-master/src/page/manage.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="manage_page fillcontain">
+		<el-row style="height: 100%;">
+	  		<el-col :span="4"  style="min-height: 100%; background-color: #324057;">
+				<el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
+					<el-menu-item ><i class="el-icon-menu"></i>管理列表</el-menu-item>
+					<el-submenu index="2">
+						<template slot="title"><i class="el-icon-document"></i>订单管理</template>
+						<el-menu-item index="userList">订单管理-平台</el-menu-item>
+						<el-menu-item index="shopList">上传物流商</el-menu-item>
+						<el-menu-item index="foodList">查看物流详情</el-menu-item>
+						<el-menu-item index="orderList">查看订单-待领取</el-menu-item>
+						<el-menu-item index="adminList">查看订单-待受理</el-menu-item>
+						<el-menu-item index="adminList">查看订单-待发货</el-menu-item>
+						<el-menu-item index="adminList">查看订单-已发货</el-menu-item>
+						<el-menu-item index="adminList">查看订单-已签收</el-menu-item>
+						<el-menu-item index="adminList">查看订单-已折现</el-menu-item>
+						<el-menu-item index="adminList">查看订单-以转增</el-menu-item>
+						<el-menu-item index="adminList">查看订单-已退回</el-menu-item>
+					</el-submenu>
+					<el-submenu index="3">
+						<template slot="title"><i class="el-icon-plus"></i>订单删除</template>
+						<el-menu-item index="addShop">订单删除提示</el-menu-item>
+						<el-menu-item index="addGoods">删除成功</el-menu-item>
+					</el-submenu>
+					<el-submenu index="4">
+						<template slot="title"><i class="el-icon-star-on"></i>线下</template>
+						<el-menu-item index="addShop">线下</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-col>
+			<el-col :span="20" style="height: 100%;overflow: auto;">
+				<keep-alive>
+				    <router-view></router-view>
+				</keep-alive>
+			</el-col>
+		</el-row>
+  	</div>
+</template>
+
+<script>
+    export default {
+		computed: {
+			defaultActive: function(){
+				return this.$route.path.replace('/', '');
+			}
+		},
+    }
+</script>
+
+
+<style lang="less" scoped>
+	@import '../style/mixin';
+	.manage_page{
+		
+	}
+</style>
---
+++ vue2-manage-master/src/page/userList.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div class="bigbox">
+<head-top></head-top>
+    <el-container>
+      <el-header class="header">订单管理/平台商品订单</el-header>
+      <el-container>
+        <el-aside class="aside" width="200px">Aside</el-aside>
+        <el-main class="main">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import headTop from '../components/headTop'
+</script>
+
+<style >
+.bigbox {
+  height: 100%;
+}
+.header {
+  width: 100%;
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+}
+.adide {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+}
+.main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+</style>
